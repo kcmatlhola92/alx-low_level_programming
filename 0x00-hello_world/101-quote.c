e4305184310f878c154b31b6f259f0db8f20abaa
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-09-08 13:54:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 /**
- * main - Program entry point
+ * main - Entry point of the program
  *
- * Return: 1 on (success)
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	59);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(2, str1, 59);
 	return (1);
 }
