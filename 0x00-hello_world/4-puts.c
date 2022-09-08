cd3671726aaf4fb889faa0ef922f3d62ac3a15a0
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-09-08 13:02:20 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Return: By defult returns zero if no erros otherwise return none-zero value.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
