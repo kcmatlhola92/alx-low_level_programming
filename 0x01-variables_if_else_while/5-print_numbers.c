398b1b096666d5edd02e623becea3dbc57987077
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-09-09 14:02:27 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers from 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return(0);
+}
