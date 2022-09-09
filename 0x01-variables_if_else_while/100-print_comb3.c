fc812ba260d3a592eb221c22ad948b10a3370dfb
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-09-09 15:03:15 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of two digits
+ *
+ * Return: zero if error and non 0 if erros
+ */
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
