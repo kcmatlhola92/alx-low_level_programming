b290e8ee584e65f4819aa4b1b633d624e7067fc9
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-09-14 05:27:38 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check for multiples of 3 and 5
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = o; num < 1024; ++num)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+		sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
