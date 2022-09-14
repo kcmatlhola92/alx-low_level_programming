19013369cb800f0f05d5aa17e931e811c7b6563d
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-09-14 05:49:16 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - print first 50 fibonacci
+ *
+ * Description: Pritns the sum of even-valued
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = fib1 + fib2
+
+		if (sum > 4000000)
+		break;
+		if ((sum % 2) == 0)
+		total_sum += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.of\n", total_sum);
+
+	return (0);
+}
