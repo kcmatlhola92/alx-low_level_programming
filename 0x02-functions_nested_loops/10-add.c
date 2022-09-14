265c34f7044987b54fc6312b0ff7f3354a542a4a
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-09-14 04:30:17 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integrers and returns the result
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: sum of a & b
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
