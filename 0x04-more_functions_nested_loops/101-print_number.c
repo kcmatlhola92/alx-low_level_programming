85dfc9505e633b31d8d441a70f72b72892a2cb9b
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-09-16 13:49:31 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * print_number - a function that prints an integer.
+ * @n: integer to be printed
+ *
+ */
+
+void print_number(int n)
+{
+	unsigned int m, d, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		while (d > 9)
+		{
+			count *= 10;
+		}
+
+		for (; count >= 1; count /= 10)
+		{
+			_putchar(((m / count) % 10) + 48);
+		}
+	}
+}
