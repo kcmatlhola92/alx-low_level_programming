2de1ec758128b9a3d13780bfdd91a3aa0a8b0b47
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-09-21 08:18:40 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src);
+void print_number(int n);
+int _putchar(char c);
+void print_buffer(char *b, int size);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+
+#endif /* MAIN_H */
