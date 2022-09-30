c43b7d67eb519cee77dc8bf3a287f685747f9ca1
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-09-30 07:49:30 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,11 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b;
-
-	if (argc == 3)
+	if (argc != 3)
 	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		printf("%d\n", a * b);
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	printf("error\n");
-	return (1);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
