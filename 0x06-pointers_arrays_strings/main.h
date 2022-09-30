8d5dda2ec58a9984888299dcd95a3f17a9d81ab8
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-09-30 12:19:27 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 
 char *_strcat(char *dest, char *src);
-void print_buffer(char *b, int size);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -14,7 +13,8 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
-int _putchar(char c);
+int _putchar(char);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif /* MAIN_H */
