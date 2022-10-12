638e776a45a7ffec2c5c56d937acb948aa1165df
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-10-12 05:29:36 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
  * print_name - a function that prints a name
  * @name: function name yto be printed
  * @f: function pointer
- * Return: void
+ * Return: no return
  */
 
 void print_name(char *name, void (*f)(char *))
