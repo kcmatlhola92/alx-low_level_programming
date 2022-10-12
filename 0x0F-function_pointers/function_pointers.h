fbeb90b2c323239bc739e16e7fd70da61a63a593
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-10-12 05:41:55 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -10,4 +10,4 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 
-#ifndef /* FUNCTION_POINTERS_H */
+#endif /* FUNCTION_POINTERS_H */
