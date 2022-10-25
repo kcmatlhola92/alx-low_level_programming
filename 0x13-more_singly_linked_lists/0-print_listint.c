1e61e9c0aa823fe291f721f333eec5cd6b148b2c
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-10-25 13:35:16 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
- * @h: the head of the list
+ * print_listint - prints all the elements of list
+ * @h: head of the list
+ *
  * Return: the number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t num = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		nnodes++;
+		num++;
 	}
-	return (nnodes);
+	return (num);
 }
