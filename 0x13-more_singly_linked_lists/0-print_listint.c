23dc0d85d4049df8ab953117be6cd90adda6582f
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-10-26 11:12:14 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t num = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		nnodes++;
+		num++;
 	}
-	return (nnodes);
+	return (num);
 }
